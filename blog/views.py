6efb509d18573bb58a592487794079ee14615836
Mars Oakley <a.oakley4589@gmail.com>
2024-11-26 15:47:37 +0000

    added Recipes app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def my_blog(request):
-    return HttpResponse("Test Message - 'Hello, Simply Sprouts!'")
+class HomePage(TemplateView):
+    """
+    Displays home page"
+    """
+    template_name = 'home.html'
