13e5ae0b8b6514c0794ae789b8e6d5a23561ccfb
Mars Oakley <a.oakley4589@gmail.com>
2024-11-28 14:18:59 +0000

    added user comments to Blog Posts, currently create and read only

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from .models import Blog_Post
+from .models import Blog_Post, Comment
+from django.contrib.auth.decorators import login_required
+from .forms import CommentForm
+from django.contrib import messages
 
 class BlogMainView(generic.ListView):
     model = Blog_Post
@@ -12,24 +15,44 @@ class BlogMainView(generic.ListView):
         return Blog_Post.objects.all().order_by('-created_on')
 
 def blog_post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Blog_Post`.
-
-    **Context**
-
-    ``blog_post``
-        An instance of :model:`blog.Blog_Post`.
-
-    **Template:**
-
-    :template:`blog/blog_post.html`
-    """
-
     queryset = Blog_Post.objects.filter(status=1)
     blog_post = get_object_or_404(queryset, slug=slug)
+    comments = blog_post.comments.all().order_by("-created_on")
+    comment_count = blog_post.comments.filter(approved=True).count()
+
+    if request.method == "POST" and request.user.is_authenticated:
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = blog_post 
+            comment.save()
+            messages.success(request, 'Your comment has been successfully submitted and is awaiting approval from our Admin team - check back soon!')
+            return redirect('blog_post_detail', slug=slug)
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
         "blog/blog_post.html",
-        {"blog_post": blog_post},
+        {
+        "blog_post": blog_post,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+        },
     )
+
+@login_required
+def add_comment(request, blog_post_id):
+    blog_post = get_object_or_404(Blog_Post, id=blog_post_id)
+    if request.method == 'POST':
+        body = request.POST.get('body')
+        if body:
+            Comment.objects.create(
+                post=blog_post,
+                author=request.user,
+                body=body,
+                approved=False
+            )
+    return redirect('blog_post_detail', slug=blog_post.slug)
