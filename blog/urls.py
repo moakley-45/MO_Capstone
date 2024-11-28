fc70687e9c044c067888997ee725c57474fb0c8c
Mars Oakley <a.oakley4589@gmail.com>
2024-11-28 11:42:44 +0000

    fixed an issue with the Blog URLs

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import BlogMainView
 
 urlpatterns = [
-    path('blog_main', BlogMainView.as_view(), name='blog main'),
-    path('/<slug:slug>/', views.blog_post_detail, name='blog_post_detail'),
+    path('', BlogMainView.as_view(), name='blog_main'),
+    path('<slug:slug>/', views.blog_post_detail, name='blog_post_detail'),
 ]
-
--- simply_sprouts/urls.py
+++ simply_sprouts/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     
     
 ]
