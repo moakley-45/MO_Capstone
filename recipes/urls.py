33097071995810095053794483837c6c78a4944f
Mars Oakley <a.oakley4589@gmail.com>
2024-12-02 11:54:51 +0000

    Updated Nav Br to link to new Cuisine Pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.RecipeView.as_view(), name='recipes_main'),
     path('<slug:slug>/', views.recipes_page, name='recipes_page'),
+    path('cuisine/<str:cuisine>/', views.cuisine_specific_recipes, name='cuisine_recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Recipe
+from .models import Recipe, CUISINE_CHOICES
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 
 class RecipeView(generic.ListView):
     model = Recipe
@@ -25,3 +26,19 @@ def recipes_page(request, slug):
         "recipe": recipe,
         },
     )
+
+def cuisine_specific_recipes(request, cuisine):
+    recipes = Recipe.objects.filter(cuisine=cuisine, status=1) 
+    return render(request, 'recipes/cuisine_specific.html', {
+        'recipes': recipes,
+        'cuisine': dict(CUISINE_CHOICES)[cuisine]
+    })
+
+    
+
+def your_view(request):
+    context = {
+        'CUISINE_CHOICES': CUISINE_CHOICES,
+        # ... other context variables ...
+    }
+    return render(request, 'cuisine_specific.html', context)
