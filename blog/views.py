d7dcc484a98b9212ee820bf7363ec40e7ec4b8bf
Mars Oakley <a.oakley4589@gmail.com>
2024-12-06 13:11:42 +0000

    minor cosmetic changes and added pagination to Blog Main Page

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ class BlogMainView(generic.ListView):
     model = Blog_Post
     template_name = "blog/blog_main.html"
     context_object_name = 'blog_posts'
-    paginate_by = 5 
+    paginate_by = 6 
 
     def get_queryset(self):
         return Blog_Post.objects.all().order_by('-created_on')
