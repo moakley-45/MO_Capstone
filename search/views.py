e3f0a5c54cc9f7a5418cbcc97f14bd79e8d86221
Mars Oakley <a.oakley4589@gmail.com>
2024-12-11 21:38:29 +0000

    Python validated for Search app

--- search/views.py
+++ search/views.py
@@ -1,19 +1,28 @@
 from django.db.models import Q
-from django.shortcuts import render
 from itertools import chain
-from recipes.models import Recipe
-from blog.models import Blog_Post
+from django.shortcuts import render
+
 
 def search_view(request):
     query = request.GET.get('q')
     combined_results = []
+
     if query:
         recipe_results = Recipe.objects.filter(
-            Q(title__icontains=query) | Q(ingredients__icontains=query) | Q(method__icontains=query)
-        )[:40] 
+            Q(title__icontains=query) |
+            Q(ingredients__icontains=query) |
+            Q(method__icontains=query)
+        )[:40]
+
         blog_results = Blog_Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        )[:50] 
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+        )[:50]
+
         combined_results = list(chain(recipe_results, blog_results))
-    
-    return render(request, 'search/search_results.html', {'results': combined_results, 'query': query})
+
+    return render(
+        request,
+        'search/search_results.html',
+        {'results': combined_results, 'query': query}
+    )
